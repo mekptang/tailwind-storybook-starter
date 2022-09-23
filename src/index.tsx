b032c01aa6036ac42e82f7a9f559d69d6b78b083
Ben Tang <bentang95@hotmail.com>
2022-09-23 17:22:15 +0800

    remove not used index

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-export * from "./@interfaces";
-export * from "./data";
-export * from "./data/images";
+// export * from "./@interfaces";
+// export * from "./data";
+// export * from "./data/images";
 export * from "./ButtonGroup";
 export * from "./ColorBox";
 export * from "./Typography";
